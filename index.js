beb7e5c667da8b3e808c7222def2be7a19d042d0
llfalcao <luizotavio-falcao@hotmail.com>
2021-08-24 04:31:16 -0300

    Remove unused functions

--- index.js
+++ index.js
@@ -132,13 +132,9 @@ function operate(sign, x, y) {
         case 'subtract':
             return subtract(x, y);
         case 'multiply':
-            return multiply([x, y]);
+            return multiply(x, y);
         case 'divide':
             return divide(x, y);
-        case '^':
-            return power(x, y);
-        case '!':
-            return factorial(x);
         default:
             break;
     }
@@ -148,43 +144,14 @@ const add = function (x, y) {
     return x + y;
 };
 
-const multiply = function (numbers) {
-    let product = 1;
-    for (let i = 0; i < numbers.length; i++) {
-        product *= numbers[i];
-    }
-    return product;
-};
-
-const divide = function (x, y) {
-    return x / y;
-};
-
 const subtract = function (x, y) {
     return x - y;
 };
 
-const sum = function (numbers) {
-    let sum = 0;
-    for (let i = 0; i < numbers.length; i++) {
-        sum += numbers[i];
-    }
-
-    return sum;
+const multiply = function (x, y) {
+    return x * y;
 };
 
-const power = function (x, y) {
-    return Math.pow(x, y);
-};
-
-const factorial = function (x) {
-    if (x === 0) {
-        return 1;
-    }
-
-    let factorial = x;
-    for (let i = x - 1; i > 0; i--) {
-        factorial *= i;
-    }
-    return factorial;
+const divide = function (x, y) {
+    return x / y;
 };
