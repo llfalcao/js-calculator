239a97eb94305b5518545658bd3e227aa4668005
Luiz Otávio Lima Falcão <luizotavio-falcao@hotmail.com>
2021-09-27 22:14:31 -0300

    Use module pattern for DOM and Display functions

--- index.js
+++ index.js
@@ -1,66 +1,93 @@
-const buttons = document.querySelectorAll('.btn');
-let operator, a, b, operandSwitch, decimalPoint;
+let operator;
+let a; // 1st operand;
+let b; // 2nd operand;
+let operandSwitch; // Defines when to clear the display for a new number
+let decimalPoint;
 let miniDisplay = [];
-// a: 1st operand;
-// b: 2nd operand;
-// operandSwitch: defines when to clear the display for a new number
 
-buttons.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        updateDisplay(btn);
-    });
-});
+// Module for DOM Elements
+const DOM = (() => {
+    const display = () => document.querySelector('.display');
+    const operation = () => document.querySelector('.operation');
+    const button = (key) => document.querySelector(`button[data-key="${key}"]`);
 
-window.addEventListener('keydown', (event) => {
-    let key = event.key.toLowerCase();
-    if (key === '/') {
-        event.preventDefault();
-    } else if (key === 'enter') {
-        event.preventDefault();
-        if (a === undefined) return;
-    } else if (key === ',') {
-        key = '.';
-    } else if (key === 'escape') {
-        key = 'c';
-    }
-    const input = document.querySelector(`button[data-key="${key}"]`);
-
-    if (input) {
-        document.addEventListener(
-            'keyup',
-            function onKeyup() {
-                document.removeEventListener('keyup', onKeyup);
-                // Default background color
-                if (input.classList.contains('equals')) {
-                    input.classList.remove('equals-selected');
-                } else if (input.classList.contains('function')) {
-                    input.classList.remove('function-selected');
-                } else {
-                    input.classList.remove('btn-selected');
-                }
-            },
-            false
-        );
-        // Background color on key press
-        if (input.classList.contains('equals')) {
-            input.classList.add('equals-selected');
-        } else if (input.classList.contains('function')) {
-            input.classList.add('function-selected');
-        } else {
-            input.classList.add('btn-selected');
+    const addListener = (type, selector, callback) => {
+        if (typeof selector === 'object') {
+            document.addEventListener(type, (e) => callback(e));
+            return;
         }
+        document.addEventListener(type, (e) => {
+            if (e.target.closest(selector)) callback(e);
+        });
+    };
+
+    return { display, operation, button, addListener };
+})();
+
+function calculate(sign, x, y) {
+    add = (x, y) => x + y;
+    sub = (x, y) => x - y;
+    mul = (x, y) => x * y;
+    div = (x, y) => x / y;
+
+    switch (sign) {
+        case 'add':
+            return this.add(x, y);
+        case 'subtract':
+            return this.sub(x, y);
+        case 'multiply':
+            return this.mul(x, y);
+        case 'divide':
+            return this.div(x, y);
+        default:
+            break;
+    }
+}
+
+function resetValues() {
+    operator = undefined;
+    a = undefined;
+    b = undefined;
+    operandSwitch = undefined;
+    decimalPoint = undefined;
+}
+
+// Module for Display Changes
+const DisplayController = (() => {
+    function handleNumber(display, currentInput) {
+        // Clear the display and start a new operation
+        // if the last button pressed was 'equals'
+        if (b !== undefined) {
+            resetValues();
+            if (currentInput === 'point') {
+                display.innerText = '0';
+            } else {
+                display.innerText = '';
+            }
+        }
+        // Clear the display when typing the second operand
+        // or just starting a new operation
+        else if (operandSwitch || display.innerText === '0') {
+            if (currentInput === 'point') {
+                display.innerText = '0';
+            } else {
+                display.innerText = '';
+            }
+        }
+        // Prevent more than one decimal point
+        if (currentInput === 'point') {
+            if (decimalPoint) {
+                return;
+            } else {
+                currentInput = '.';
+                decimalPoint = true;
+            }
+        }
+        operandSwitch = false;
+        display.innerText += currentInput;
     }
 
-    try {
-        updateDisplay(input);
-    } catch (error) {}
-});
-
-function updateDisplay(input) {
-    const display = document.querySelector('.display');
-    let result = 0;
-
-    if (input.classList.contains('operator')) {
+    function handleOperator(display, input, result) {
         // Change operation without affecting the current operand
         if (operator !== undefined && operandSwitch) {
             b = undefined;
@@ -86,7 +113,9 @@ function updateDisplay(input) {
         operator = input.id;
         operandSwitch = true;
         decimalPoint = false;
-    } else if (input.classList.contains('equals')) {
+    }
+
+    function handleEquals(display, result) {
         if (b === undefined || !operandSwitch) {
             b = parseFloat(display.innerText);
             if (operator === 'divide' && b === 0) {
@@ -95,50 +124,33 @@ function updateDisplay(input) {
                 return;
             }
         }
-
         result = Math.round(calculate(operator, a, b) * 1e9) / 1e9;
         display.innerText = result;
         a = result;
         operandSwitch = true;
-    } else if (input.classList.contains('clear')) {
-        display.innerText = '0';
-        operator = a = b = operandSwitch = decimalPoint = undefined;
-    } else {
-        let currentInput = input.id;
-
-        // Clear the display and start a new operation
-        // if the last button pressed was 'equals'
-        if (b !== undefined) {
-            operator = a = b = operandSwitch = decimalPoint = undefined;
-            if (currentInput === 'point') {
-                display.innerText = '0';
-            } else {
-                display.innerText = '';
-            }
-        }
-        // Clear the display when typing the second operand
-        // or just starting a new operation
-        else if (operandSwitch || display.innerText === '0') {
-            if (currentInput === 'point') {
-                display.innerText = '0';
-            } else {
-                display.innerText = '';
-            }
-        }
-
-        if (currentInput === 'point') {
-            if (decimalPoint) {
-                return;
-            } else {
-                currentInput = '.';
-                decimalPoint = true;
-            }
-        }
-        operandSwitch = false;
-        display.innerText += currentInput;
     }
 
-    // Update upper area of the display containing the current operation
+    return { handleNumber, handleOperator, handleEquals };
+})();
+
+function updateDisplay(input) {
+    const display = DOM.display();
+    let result = 0;
+
+    // Update Main Display
+    if (input.classList.contains('operator')) {
+        DisplayController.handleOperator(display, input, result);
+    } else if (input.classList.contains('equals')) {
+        DisplayController.handleEquals(display, result);
+    } else if (input.classList.contains('clear')) {
+        display.innerText = '0';
+        resetValues();
+    } else {
+        let currentInput = input.id;
+        DisplayController.handleNumber(display, currentInput);
+    }
+
+    // Update Mini Display
     if (!operandSwitch && operator === undefined) {
         miniDisplay[0] = display.innerText;
     } else {
@@ -151,25 +163,55 @@ function updateDisplay(input) {
     if (a !== undefined && !operandSwitch) {
         miniDisplay[2] = display.innerText;
     }
-    document.querySelector('.operation').innerText = miniDisplay.join(' ');
+    DOM.operation().innerText = miniDisplay.join(' ');
 }
 
-function calculate(sign, x, y) {
-    add = (x, y) => x + y;
-    sub = (x, y) => x - y;
-    mul = (x, y) => x * y;
-    div = (x, y) => x / y;
+// Handle Button Clicks
+DOM.addListener('click', '.btn', (e) => {
+    updateDisplay(e.target);
+});
 
-    switch (sign) {
-        case 'add':
-            return this.add(x, y);
-        case 'subtract':
-            return this.sub(x, y);
-        case 'multiply':
-            return this.mul(x, y);
-        case 'divide':
-            return this.div(x, y);
-        default:
-            break;
+// Handle Button Key Presses
+DOM.addListener('keydown', window, (event) => {
+    let key = event.key.toLowerCase();
+    if (key === '/') {
+        event.preventDefault();
+    } else if (key === 'enter') {
+        event.preventDefault();
+        if (a === undefined) return;
+    } else if (key === ',') {
+        key = '.';
+    } else if (key === 'escape') {
+        key = 'c';
     }
-}
+
+    const input = DOM.button(key);
+    if (input) {
+        // Change button color on key press
+        if (input.classList.contains('equals')) {
+            input.classList.add('equals-selected');
+        } else if (input.classList.contains('function')) {
+            input.classList.add('function-selected');
+        } else {
+            input.classList.add('btn-selected');
+        }
+    }
+
+    try {
+        updateDisplay(input);
+    } catch (error) {}
+});
+
+DOM.addListener('keyup', window, (event) => {
+    // Revert to the button's original color
+    const input = DOM.button(event.key);
+    if (input) {
+        if (input.classList.contains('equals')) {
+            input.classList.remove('equals-selected');
+        } else if (input.classList.contains('function')) {
+            input.classList.remove('function-selected');
+        } else {
+            input.classList.remove('btn-selected');
+        }
+    }
+});
